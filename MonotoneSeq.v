1507fc55e090e21146902a5db7de595345ac1485
Antonin Delpeuch <antonin@delpeuch.eu>
2019-04-18 17:32:45 +0100

    Addition for left reals

--- MonotoneSeq.v
+++ MonotoneSeq.v
@@ -42,6 +42,8 @@ Section LeftReals.
     assumption.
   Qed.
 
+  Definition Qhalf x : Q := x / inject_Z 2.
+
   Lemma Q_times_two : forall x : Q, x + x = x * inject_Z 2.
   Proof.
     intros. unfold Qplus, Qmult.
@@ -53,31 +55,42 @@ Section LeftReals.
     ring.
   Qed.
 
+  Lemma Qhalf_split : forall x : Q, Qhalf x + Qhalf x = x.
+  Proof.
+    intros. unfold Qhalf.
+    rewrite Q_times_two.
+    rewrite Qmult_comm.
+    rewrite Qmult_div_r.
+    reflexivity.
+    unfold inject_Z.
+    unfold Qeq. simpl.
+    lia.
+  Qed.
+
+  Lemma Qhalf_pos : forall x, x > 0 -> Qhalf x > 0.
+  Proof.
+    unfold Qhalf.
+    intros.
+    apply Qlt_shift_div_l.
+    unfold Qlt, Zlt. simpl. reflexivity.
+    rewrite Qmult_0_l. assumption.
+  Qed.
+
   Lemma LRle_trans : forall x y z, LRle x y -> LRle y z -> LRle x z.
   Proof.
     unfold LRle ; intros.
-    set (u := ϵ / (inject_Z 2)).
-    assert (ϵ = u + u).
-    - unfold u.
-      rewrite Q_times_two.
-      rewrite Qmult_comm.
-      rewrite Qmult_div_r.
-      reflexivity.
-      unfold inject_Z, Qeq. simpl.
-      lia.
+    set (u := Qhalf ϵ).
+    pose proof (Qhalf_split ϵ) as Hsplit.
     - assert (0 < u).
-      unfold u.
-      apply Qlt_shift_div_l.
-      unfold Qlt, Zlt. simpl. reflexivity.
-      rewrite Qmult_0_l. assumption.
-      pose proof (H u H3 n) as H.
+      apply Qhalf_pos. assumption.
+      pose proof (H u H2 n) as H.
       destruct H.
-      pose proof (H0 u H3 x0) as H0.
+      pose proof (H0 u H2 x0) as H0.
       destruct H0.
       exists x1.
       apply Qle_trans with (y := (` y) x0 + u).
       assumption.
-      rewrite H2.
+      rewrite <- Hsplit.
       rewrite Qplus_assoc.
       apply Qplus_le_compat.
       assumption.
@@ -91,8 +104,9 @@ Section LeftReals.
       LRle_refl
       LRle_trans.
   Existing Instance PO_for_LR.
+  Instance LRequiv : Equiv LReal := Feq.
 
-  Lemma LR_plus_defined : forall x y : LReal,
+  Lemma LRplus_defined : forall x y : LReal,
       monotone (fun n => ((` x) n + (` y) n)).
   Proof.
     unfold monotone; intros.
@@ -101,17 +115,80 @@ Section LeftReals.
     apply (proj2_sig y n m H).
   Qed.
 
-  Definition LR_plus (x y : LReal) : LReal :=
-    exist monotone (fun n => (` x) n + (` y) n) (LR_plus_defined x y).
+  Definition LRplus (x y : LReal) : LReal :=
+    exist monotone (fun n => (` x) n + (` y) n) (LRplus_defined x y).
 
-  Instance LR_plus_pomorph2 : POMorphism2 LR_plus.
+  Instance LRplus_pomorph2 : POMorphism2 LRplus.
   Proof.
     apply MkPOMorphism2.
-    unfold LR_plus.
-    intros.
-    unfold "≤", LRle_le, LRle.
+    unfold LRplus, "≤", LRle_le, LRle.
     intros. simpl.
-    
+    set (u := Qhalf ϵ).
+    assert (0 < u).
+    apply (Qhalf_pos ϵ H1).
+    pose proof (H u H2 n) as H.
+    destruct H.
+    pose proof (H0 u H2 n) as H0.
+    destruct H0.
+    set (p := max x0 x1).
+    pose proof (Max.le_max_l x0 x1) as M1.
+    pose proof (Max.le_max_r x0 x1) as M2.
+    pose proof (proj2_sig y x0 p M1) as x0_p.
+    pose proof (proj2_sig b x1 p M2) as x1_p.
+    exists p.
+    rewrite <- (Qhalf_split ϵ).
+    rewrite <- Qplus_le_l with (z := u) in x0_p.
+    rewrite <- Qplus_le_l with (z := u) in x1_p.
+    pose proof (Qle_trans _ _ _ H x0_p).
+    pose proof (Qle_trans _ _ _ H0 x1_p).
+    rewrite Qplus_assoc.
+    apply (Qplus_le_compat _ _ _ _ H3) in H4.
+    rewrite Qplus_assoc in H4.
+    rewrite (Qplus_comm _ ((` b) p)) in H4.
+    rewrite Qplus_assoc in H4.
+    unfold u in H4.
+    rewrite (Qplus_comm ((`y) p)).
+    assumption.
+  Qed.
+
+  Instance LRplus_plus : Plus LReal := LRplus.
+
+  Lemma Q_to_LR_def (x : Q) : monotone (fun n => x).
+  Proof.
+    unfold monotone; intros.
+    apply Qle_refl.
+  Qed.
+
+  Definition Q_to_LR (x : Q) :=
+    exist monotone (fun n => x) (Q_to_LR_def x).
+
+  Instance Q_to_LR_pomorph : POMorphism Q_to_LR.
+  Proof.
+    apply MkPOMorphism.
+    unfold Q_to_LR, "≤", LRle_le, LRle; intros. simpl.
+    exists n.
+    apply Qlt_le_weak.
+    rewrite Qplus_comm.
+    rewrite <- (Qplus_0_l x).
+    apply Qplus_lt_le_compat ; assumption.
+  Qed.
+
+  Definition LRzero : LReal := Q_to_LR 0.
+  Instance LRzero_zero : Zero LReal := LRzero.
+
+  Lemma LRplus_0_l : forall x : LReal, LRplus LRzero x = x.
+  Proof.
+    unfold LRplus, LRzero, "=", LRequiv, Feq, "≤", LRle_le, LRle. intros.
+    simpl. split ; intros ; exists n ; rewrite Qplus_0_l ;
+             apply Qlt_le_weak ; rewrite Qplus_comm ;
+               rewrite <- (Qplus_0_l ((`x) n)) at 1 ;
+               apply Qplus_lt_le_compat.
+    assumption.
+    apply Qle_refl.
+    assumption.
+    apply Qle_refl.
+  Qed.         
+  
 End LeftReals.
       
       
